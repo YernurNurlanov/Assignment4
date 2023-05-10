9f1a6b0f4418d0d47918585fb668e2eda7a2574f
Ернур Нурланов <nurlanovernur33@gmail.com>
2023-05-10 09:31:40 +0600

    Implement HashNode<K, V> class.

--- src/MyHashTable.java
+++ src/MyHashTable.java
@@ -1,3 +1,14 @@
 public class MyHashTable<K, V>{
-
+    private static class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+        public String toString(){
+            return "{" + key + " " + value + "}";
+        }
+    }
 }
